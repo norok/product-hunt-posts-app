2af7902e6104bc5fd3451d39d6460233a43d7e8d
norok <thexsonic@gmail.com>
2025-06-16 18:26:42 -0300

    feat: add vitest basic configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import { Header, Tabs, Posts } from "./components";
 import styles from "./App.module.scss";
+import { Provider } from "react-redux";
+import { store } from "./app/store.ts";
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Header />
       <main className={styles.main}>
         <Tabs />
         <Posts />
       </main>
-    </>
+    </Provider>
   )
 }
 
